819da5222f77d11273a27a6955abd06868a0581b
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:54:09 +0400

    Update bottomnav.js

--- webapp/components/bottomnav.js
+++ webapp/components/bottomnav.js
@@ -1,1 +1,15 @@
+import { navigate } from "../router.js"
 
+export function BottomNav(){
+
+ return `
+ <div class="nav">
+
+  <button onclick="navigate('home')">Главная</button>
+  <button disabled>Лидерборд</button>
+  <button onclick="navigate('profile')">Профиль</button>
+
+ </div>
+ `
+
+}
