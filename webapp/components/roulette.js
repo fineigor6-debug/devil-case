8eb38740e5a3273e35d4db41b60976cccb5d45d4
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:54:52 +0400

    Update roulette.js

--- webapp/components/roulette.js
+++ webapp/components/roulette.js
@@ -1,1 +1,24 @@
+export function Roulette(){
 
+ const items = [
+  "NFT Cat",
+  "NFT Dog",
+  "NFT Demon",
+  "NFT Devil"
+ ]
+
+ const item = items[Math.floor(Math.random()*items.length)]
+
+ return `
+ <div class="roulette">
+
+   <p>🎰 Рулетка</p>
+
+   <button onclick="alert('Вы выбили: ${item}')">
+   Открыть кейс
+   </button>
+
+ </div>
+ `
+
+}
