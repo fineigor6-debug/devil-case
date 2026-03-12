dbcf4cd5c1721f424ae8f1556a5d0d2b1b7281fb
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:55:52 +0400

    Update casesscreen.js

--- webapp/screens/casesscreen.js
+++ webapp/screens/casesscreen.js
@@ -1,1 +1,17 @@
+import { Header } from "../components/Header.js"
+import { CaseCard } from "../components/CaseCard.js"
+import { BottomNav } from "../components/BottomNav.js"
 
+export function renderCases(app){
+
+ app.innerHTML = `
+
+ ${Header("Кейсы", true)}
+
+ ${CaseCard()}
+
+ ${BottomNav()}
+
+ `
+
+}
