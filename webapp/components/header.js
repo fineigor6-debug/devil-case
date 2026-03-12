0946a832579f335f5a62df67d6cdb9be7746540a
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:53:52 +0400

    Update header.js

--- webapp/components/header.js
+++ webapp/components/header.js
@@ -1,1 +1,13 @@
+export function Header(title, back){
 
+ return `
+  <div class="header">
+
+   ${back ? `<button onclick="history.back()">←</button>` : ""}
+
+   <h2>${title}</h2>
+
+  </div>
+ `
+
+}
