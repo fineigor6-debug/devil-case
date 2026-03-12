a92cc5f84910180293fb8421a9915c050cdb3542
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:55:33 +0400

    Update homescreen.js

--- webapp/screens/homescreen.js
+++ webapp/screens/homescreen.js
@@ -1,1 +1,31 @@
+import { navigate } from "../router.js"
+import { BottomNav } from "../components/BottomNav.js"
 
+export function renderHome(app){
+
+ app.innerHTML = `
+
+ <div class="header">
+  <h2>Devil Case</h2>
+ </div>
+
+ <div class="menu">
+
+  <button onclick="navigate('cases')">
+   Кейсы
+  </button>
+
+  <button disabled>
+   Крафт (в разработке)
+  </button>
+
+  <button disabled>
+   Ракета (в разработке)
+  </button>
+
+ </div>
+
+ ${BottomNav()}
+ `
+
+}
