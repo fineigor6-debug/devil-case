a3a5c2ade8c6a57f429f54084d4ebf07469c34ec
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:56:14 +0400

    Update caseopenscreen.js

--- webapp/screens/caseopenscreen.js
+++ webapp/screens/caseopenscreen.js
@@ -1,1 +1,24 @@
+import { Header } from "../components/Header.js"
+import { Roulette } from "../components/Roulette.js"
+import { BottomNav } from "../components/BottomNav.js"
 
+export function renderCase(app){
+
+ app.innerHTML = `
+
+ ${Header("Тестовый кейс", true)}
+
+ ${Roulette()}
+
+ <p>Дроп:
+ NFT Cat
+ NFT Dog
+ NFT Demon
+ NFT Devil
+ </p>
+
+ ${BottomNav()}
+
+ `
+
+}
