07147c419ecb00655bb8fc3bc1167e48b54f7f8c
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:53:28 +0400

    Update router.js

--- webapp/router.js
+++ webapp/router.js
@@ -1,1 +1,17 @@
+import { renderHome } from "./screens/HomeScreen.js"
+import { renderCases } from "./screens/CasesScreen.js"
+import { renderCase } from "./screens/CaseOpenScreen.js"
+import { renderProfile } from "./screens/ProfileScreen.js"
 
+export function navigate(screen){
+
+ const app = document.getElementById("app")
+
+ app.innerHTML = ""
+
+ if(screen === "home") renderHome(app)
+ if(screen === "cases") renderCases(app)
+ if(screen === "case") renderCase(app)
+ if(screen === "profile") renderProfile(app)
+
+}
