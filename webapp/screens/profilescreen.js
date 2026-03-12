bc0b2e45784b995fd8ce4737a0f031ccabdc250c
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:56:31 +0400

    Update profilescreen.js

--- webapp/screens/profilescreen.js
+++ webapp/screens/profilescreen.js
@@ -1,1 +1,31 @@
+import { BottomNav } from "../components/BottomNav.js"
+import { InventoryItem } from "../components/InventoryItem.js"
 
+export function renderProfile(app){
+
+ app.innerHTML = `
+
+ <div class="header"></div>
+
+ <div class="profile">
+
+  <img src="https://placehold.co/100">
+
+  <h3>Player</h3>
+
+  <p>ID: 1001</p>
+
+  <p>Balance: 0 ⭐</p>
+
+  <h4>Inventory</h4>
+
+  ${InventoryItem("NFT Cat")}
+  ${InventoryItem("NFT Demon")}
+
+ </div>
+
+ ${BottomNav()}
+
+ `
+
+}
