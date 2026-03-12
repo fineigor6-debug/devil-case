c5249d849c722f9af528e61555b60e6b5fea6af7
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 20:54:33 +0400

    Update casecard.js

--- webapp/components/casecard.js
+++ webapp/components/casecard.js
@@ -1,1 +1,17 @@
+import { navigate } from "../router.js"
 
+export function CaseCard(){
+
+ return `
+ <div class="case-card">
+
+   <h3>Тестовый кейс</h3>
+
+   <button onclick="navigate('case')">
+   Открыть
+   </button>
+
+ </div>
+ `
+
+}
